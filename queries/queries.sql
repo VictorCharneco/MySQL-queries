f29e88f3b1ae4e7ce705c455b139cf214b96be63
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-01 19:37:43 +0100

    Remove ORDER BY clause from producto query

--- queries/queries.sql
+++ queries/queries.sql
@@ -5,7 +5,7 @@ SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-SELECT codigo, nombre, precio, codigo_fabricante FROM producto ORDER BY nombre ASC;
+SELECT codigo, nombre, precio, codigo_fabricante FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
 SELECT nombre, precio AS precio_eur, precio * 1 AS precio_usd FROM producto;
